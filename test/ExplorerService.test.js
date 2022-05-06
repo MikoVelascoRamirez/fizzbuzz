33423505bab214bd4487a4af69ca919d519594a5
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-05-06 14:48:49 -0500

    11) feat: re-factoring filteringByMission() with it's own test

--- test/ExplorerService.test.js
+++ test/ExplorerService.test.js
@@ -1,4 +1,6 @@
 const ExplorerService = require('../lib/services/ExplorerService');
+const jsonExplorers = require('../lib/utils/Reader');
+const Explorers = jsonExplorers.readJsonfile('explorers.json');
 
 describe("Tests's suite for ExplorerService class", () => {
     test("1) Test if the params methods are not the correct", () => {
@@ -11,4 +13,12 @@ describe("Tests's suite for ExplorerService class", () => {
         expect(amountOfExplorers).toBe("Los parámetros no son los correctos");
         expect(explorersUsernames).toBe("Los parámetros no son los correctos");
     })
+
+    test("2) Getting an array with the explorers on Node", () => {
+        const explorersInNode = ExplorerService.filterByMission(Explorers, "node"); 
+
+        for(let indexExplorer = 0; indexExplorer < explorersInNode.length; indexExplorer++){
+            expect(explorersInNode[indexExplorer].mission).toBe("node");
+        }
+    });
 });
