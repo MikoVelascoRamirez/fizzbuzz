25469cca3c09a16a5c06e2657836fd68de2d42a4
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-05-09 16:55:40 -0500

    33) chore: re-configuring linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
         "es2021": true
     },
-    "extends": [
-        "airbnb-base"
-    ],
+    "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest"
     },
