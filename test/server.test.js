4b549742a60b1c0d38189134df1514eead6dcba9
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-05-11 13:42:35 -0500

    42) test: testing if endpoint GET /V1/explorers/:mission return a list with the explorers

--- test/server.test.js
+++ test/server.test.js
@@ -6,4 +6,9 @@ describe("Tests's suite of API Server", () => {
         const response = await request(app).get("/");
         expect(response.status).toBe(200);
     });
+
+    test("Test if endpoint GET /v1/explorers/:mission returns a list with explorers", async () => {
+        const response = await request(app).get("/v1/explorers/node").send();
+        expect(response._body.explorers.length).not.toBe(0);
+    });
 });
